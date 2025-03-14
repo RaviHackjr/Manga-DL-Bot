6031f84ff5a0507d72b262b3e5df09377f1254f9
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-03-14 19:59:38 +0530

    real code

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import asyncio as aio
 from bot import *
-from web import app  # Flask app import kiya
+
 
 async def async_main():
     db = DB()
     await db.connect()
-
-    # Flask ko async mode me run karo
-    loop = aio.get_running_loop()
-    loop.create_task(run_flask())
-
+    
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(async_main())
@@ -17,12 +13,3 @@ if __name__ == '__main__':
     for i in range(10):
         loop.create_task(chapter_creation(i + 1))
     bot.run()
-
-# Flask server async function
-async def run_flask():
-    from hypercorn.asyncio import serve
-    from hypercorn.config import Config
-
-    config = Config()
-    config.bind = ["0.0.0.0:8000"]  # Koyeb expects 8000
-    await serve(app, config)
